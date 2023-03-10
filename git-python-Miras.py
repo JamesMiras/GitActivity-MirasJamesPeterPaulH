6b3c28f905bb43dca17cf02626474b65c226790f
JamesMiras <james.properties.miras@gmail.com>
2023-03-10 16:56:08 +0800

    My Educational Background added

--- git-python-Miras.py
+++ git-python-Miras.py
@@ -1,3 +1,8 @@
+#My Personal Information
 print ("James Peter Paul H. Miras")
 print ("Nangka I, Mogpog, Marinduque")
 print ("March 14 2003")
+#My Educational background
+print ("College of Information in Computer Sciences")
+print ("Bachelor of Science in Information Technology")
+print ("BSI/T 2B")
